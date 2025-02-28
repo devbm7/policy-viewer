6f60e4194ab0eab9eade0843a5a61ac4491cb442
Dev Makwana <devmakwana7704@gmail.com>
2025-02-28 06:32:08 +0530

    v0.3.11

--- pages/index.tsx
+++ pages/index.tsx
@@ -364,7 +364,7 @@ export default function Home() {
         </div>
         {sidebarOpen && (
           <>
-            <div className="p-4">
+            <div className="p-4 overflow-y-auto">
               <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
                 onClick={() => setShowRaw(!showRaw)}
