06a3f3f3a0c26f79ba4cb7f87ae7aad87e20a40f
Dev Makwana <devmakwana7704@gmail.com>
2025-03-18 15:41:38 +0530

    v0.4.19 - seeing if this works

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 const nextConfig = {
   reactStrictMode: true,
 }
+module.exports = {
+  redirects() {
+    return [
+      process.env.MAINTENANCE_MODE === "1"
+        ? { source: "/((?!maintenance).*)", destination: "/maintenance.html", permanent: false }
+        : null,
+    ].filter(Boolean);
+  },
+};
 
 module.exports = nextConfig
 
